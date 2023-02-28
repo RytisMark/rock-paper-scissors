09cb6793b372709535ff6407580d6198906d66c5
RytisMark <rytismarkeviciuss@gmail.com>
2023-02-28 17:46:21 +0200

    half-done UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
@@ -6,70 +9,78 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection= computerSelection.toLowerCase();
+    const roundInfo = document.querySelector('.round-info');
 
     switch(playerSelection) {
         case "rock":
             if (computerSelection === "rock") {
+                roundInfo.textContent = "The round was a draw.";
                 return "draw";
             }
             else if (computerSelection === "paper") {
+                roundInfo.textContent = "You lost the round..."
                 return "lose";
             }
             else if (computerSelection === "scissors") {
-                return "won";
+                roundInfo.textContent = "You won the round!";
+                return "win";
             }
         case "paper":
             if (computerSelection === "rock") {
-                return "won";
+                roundInfo.textContent = "You won the round!";
+                return "win";
             }
             else if (computerSelection === "paper") {
+                roundInfo.textContent = "The round was a draw.";
                 return "draw";
             }
             else if (computerSelection === "scissors") {
+                roundInfo.textContent = "You lost the round..."
                 return "lose";
             }
         case "scissors":
             if (computerSelection === "rock") {
+                roundInfo.textContent = "You lost the round..."
                 return "lose";
             }
             else if (computerSelection === "paper") {
-                return "won";
+                roundInfo.textContent = "You won the round!";
+                return "win";
             }
             else if (computerSelection === "scissors") {
+                roundInfo.textContent = "The round was a draw.";
                 return "draw";
             }
     }
 }
 
-function game() {
-    let playerCount = 0;
-    let computerCount = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What will you choose? Rock, Paper or Scissors?");
-        const computerSelection = getComputerChoice();
-        const outcome = playRound(playerSelection, computerSelection);
-        if (outcome === "won") {
-            playerCount++;
-            console.log("You won the round! Current points: you - " + playerCount + ", computer - " + computerCount);
-        }
-        else if (outcome === "lose") {
-            computerCount++;
-            console.log("You lost the round! Current points: you - " + playerCount + ", computer - " + computerCount);
-        }
-        else {
-            console.log("It's a draw! Current points: you - " + playerCount + ", computer - " + computerCount);
-        }
-    }
-    if (computerCount > playerCount) {
-        console.log("The computer won.");
-    }
-    else if (playerCount > computerCount) { 
-        console.log("You won!");
-    }
-    else {
-        console.log("It's a draw.");
+function updateInfo(outcome) {
+    const scoreboard = document.querySelector('.scoreboard');
+    const winnerInfo = document.querySelector('.winner-info');
+    
+    if (outcome === "win") playerScore += 1;
+    else if (outcome === "lose") computerScore += 1;
+
+    scoreboard.textContent = `Current score: ${playerScore} (you) - ${computerScore} (computer)`;
+
+    if ((computerScore >= 5) && (computerScore > playerScore)) {
+        winnerInfo.textContent = "The computer won the game...";
+    } else if ((playerScore >= 5) && (playerScore > computerScore)) { 
+        winnerInfo.textContent = "You won the game!";
     }
 }
 
-
-game();
+const btns = document.querySelectorAll('button');
+btns.forEach(function(btn) {
+    btn.addEventListener('click', (e) => {
+        if (e.srcElement.className === 'btn-rock') {
+            updateInfo(playRound("rock", getComputerChoice()));
+        }
+        else if (e.srcElement.className === 'btn-paper') {
+            updateInfo(playRound("paper", getComputerChoice()));
+        }
+        else if (e.srcElement.className === 'btn-scissors') {
+            updateInfo(playRound("scissors", getComputerChoice()));
+        }
+    });
+});
